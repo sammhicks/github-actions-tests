bbfc5f6aa42d2a7bbffef04afdac5ee833d09ba5
Samuel Hicks <SamMHicks@hotmail.co.uk>
2020-08-02 19:09:52 +0100

    Added tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_pass() {
+        assert_eq!(1 + 2, 3);
+    }
+
+    #[test]
+    fn test_bad_add() {
+        assert_eq!(1 + 2, 4);
+    }
+}
